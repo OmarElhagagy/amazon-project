fe91f0441d26ba59b08e5fba84db22c715c7e00f
OmarElhagagy <oadeell21@gmail.com>
2024-10-15 20:54:08 +0300

    use products.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -10,6 +10,7 @@ so we need to take this data and save it inside our js file so our js can use it
 */
 //this is called data structure
 // 1- SAVE THE DATA
+/*
 const products = [{
   image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
   name:  'Black and Gray Athletic Cotton Socks - 6 Pairs',
@@ -34,7 +35,16 @@ const products = [{
     count: 56 
   },
   priceCents: 799 
+}, {
+  image: 'images/products/black-2-slot-toaster.jpg',
+  name: '2 Slot Toaster - Black',
+  rating: {
+    stars: 5.0,
+    count: 2197
+  },
+  priceCents: 1899
 }];
+*/
 // to generate the html we can loop through this array and for each of this products we are going to create some html
 // 2- GENERATE HTML
 //we will combine all this html together
@@ -97,5 +107,7 @@ products.forEach((product) => { // so the way forEach works is that it takes eac
 console.log(productsHTML); // we combined all the html for all the products together
 // last step is to take all this html and put it on the webpage using the DOM which is the document object model
 // we are going to use the DOM to take this element <div class="products-grid"> put it in our JS and replace all the HTML inside
-
+//BENEFITS of generating HTML with JS is if i we want to add anothe product we just add it to the array and it will automatically generate the HTML for us
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+// we deleted the array we had here as we have an array in products.js file and amazon.js loops through the products and creates the HTML for each product
+// our html has 2 js files one wit the products and one with the js code that generates the html
