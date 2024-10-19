31acb615c3f499fb22d67eaefd257af3e13ec86b
OmarElhagagy <oadeell21@gmail.com>
2024-10-19 16:05:45 +0300

    solved the delete button in checkout

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -19,157 +19,84 @@ cart.forEach((cartItem) => {
   // we are using the same code in checkout.js and amazon.js we can share this code between the two files by modules
   // we wont need / 100).toFixed(2) part as we made a function for it in another file
   // instead of using delivery-option-1 we will use delivery-option-${product.id} so that each product has its own delivery options
-  cartSummaryHTML +=`
-     <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
-        <div class="delivery-date">
-          Delivery date: Tuesday, June 21
+  cartSummaryHTML += `
+    <div class="cart-item-container
+      js-cart-item-container-${matchingProduct.id}">
+      <div class="delivery-date">
+        Delivery date: Tuesday, June 21
+      </div>
+
+      <div class="cart-item-details-grid">
+        <img class="product-image"
+          src="${matchingProduct.image}">
+
+        <div class="cart-item-details">
+          <div class="product-name">
+            ${matchingProduct.name}
+          </div>
+          <div class="product-price">
+            $${formatCurrency(matchingProduct.priceCents)}
+          </div>
+          <div class="product-quantity">
+            <span>
+              Quantity: <span class="quantity-label">${cartItem.quantity}</span>
+            </span>
+            <span class="update-quantity-link link-primary">
+              Update
+            </span>
+            <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
+              Delete
+            </span>
+          </div>
         </div>
 
-        <div class="cart-item-details-grid">
-          <img class="product-image"
-            src="${matchingProduct.image}">
-
-          <div class="cart-item-details">
-            <div class="product-name">
-              ${matchingProduct.name}
-            </div>
-            <div class="product-price">
-              $${formatCurrency(matchingProduct.priceCents)} 
-            </div>
-            <div class="product-quantity">
-              <span>
-                Quantity: <span class="quantity-label">${cartItem.quantity}</span>
-              </span>
-              <span class="update-quantity-link link-primary">
-                Update
-              </span>
-              <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
-                Delete
-              </span>
+        <div class="delivery-options">
+          <div class="delivery-options-title">
+            Choose a delivery option:
+          </div>
+          <div class="delivery-option">
+            <input type="radio" checked
+              class="delivery-option-input"
+              name="delivery-option-${matchingProduct.id}">
+            <div>
+              <div class="delivery-option-date">
+                Tuesday, June 21
+              </div>
+              <div class="delivery-option-price">
+                FREE Shipping
+              </div>
             </div>
           </div>
-
-          <div class="delivery-options">
-            <div class="delivery-options-title">
-              Choose a delivery option:
-            </div>
-            <div class="delivery-option">
-              <input type="radio" checked
-                class="delivery-option-input"
-                name="delivery-option-${matchingProduct.id}">
-              <div>
-                <div class="delivery-option-date">
-                  Tuesday, June 21
-                </div>
-                <div class="delivery-option-price">
-                  FREE Shipping
-                </div>
+          <div class="delivery-option">
+            <input type="radio"
+              class="delivery-option-input"
+              name="delivery-option-${matchingProduct.id}">
+            <div>
+              <div class="delivery-option-date">
+                Wednesday, June 15
+              </div>
+              <div class="delivery-option-price">
+                $4.99 - Shipping
               </div>
             </div>
-            <div class="delivery-option">
-              <input type="radio"
-                class="delivery-option-input"
-                name="delivery-option-${matchingProduct.id}">
-              <div>
-                <div class="delivery-option-date">
-                  Wednesday, June 15
-                </div>
-                <div class="delivery-option-price">
-                  $4.99 - Shipping
-                </div>
+          </div>
+          <div class="delivery-option">
+            <input type="radio"
+              class="delivery-option-input"
+              name="delivery-option-${matchingProduct.id}">
+            <div>
+              <div class="delivery-option-date">
+                Monday, June 13
               </div>
-            </div>
-            <div class="delivery-option">
-              <input type="radio"
-                class="delivery-option-input"
-                name="delivery-option-${matchingProduct.id}">
-              <div>
-                <div class="delivery-option-date">
-                  Monday, June 13
-                </div>
-                <div class="delivery-option-price">
-                  $9.99 - Shipping
-                </div>
+              <div class="delivery-option-price">
+                $9.99 - Shipping
               </div>
             </div>
           </div>
         </div>
       </div>
-
-      <div class="cart-item-container">
-        <div class="delivery-date">
-          Delivery date: Wednesday, June 15
-        </div>
-
-        <div class="cart-item-details-grid">
-          <img class="product-image"
-            src="images/products/intermediate-composite-basketball.jpg">
-
-          <div class="cart-item-details">
-            <div class="product-name">
-              Intermediate Size Basketball
-            </div>
-            <div class="product-price">
-              $20.95
-            </div>
-            <div class="product-quantity">
-              <span>
-                Quantity: <span class="quantity-label">1</span>
-              </span>
-              <span class="update-quantity-link link-primary">
-                Update
-              </span>
-              <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
-                Delete
-              </span>
-            </div>
-          </div>
-
-          <div class="delivery-options">
-            <div class="delivery-options-title">
-              Choose a delivery option:
-            </div>
-
-            <div class="delivery-option">
-              <input type="radio" class="delivery-option-input"
-                name="delivery-option-2">
-              <div>
-                <div class="delivery-option-date">
-                  Tuesday, June 21
-                </div>
-                <div class="delivery-option-price">
-                  FREE Shipping
-                </div>
-              </div>
-            </div>
-            <div class="delivery-option">
-              <input type="radio" checked class="delivery-option-input"
-                name="delivery-option-2">
-              <div>
-                <div class="delivery-option-date">
-                  Wednesday, June 15
-                </div>
-                <div class="delivery-option-price">
-                  $4.99 - Shipping
-                </div>
-              </div>
-            </div>
-            <div class="delivery-option">
-              <input type="radio" class="delivery-option-input"
-                name="delivery-option-2">
-              <div>
-                <div class="delivery-option-date">
-                  Monday, June 13
-                </div>
-                <div class="delivery-option-price">
-                  $9.99 - Shipping
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-  `
+    </div>
+  `;
 })
 
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
