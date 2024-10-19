762013f2be0fd13ec28eac4e43d3e5564932d17c
OmarElj <166073351+OmarElhagagy@users.noreply.github.com>
2024-10-19 18:00:57 +0300

    Update comments

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,11 +1,11 @@
 import {cart, removeFromCart} from '../data/cart.js'; // we will loop through the cart and generate the html
 import { products } from '../data/products.js';
-import {formatCurrency} from './utils/money.js' //./ means in the same directory
+import {formatCurrency} from './utils/money.js' 
 
-let cartSummaryHTML = ''; // we will save the html in this variable
+let cartSummaryHTML = ''; // save the html in this variable
 
 cart.forEach((cartItem) => {
-  const productId = cartItem.productId; // we will get the product id from the cartItem and we will use it to search for the full product
+  const productId = cartItem.productId; // get the product id from the cartItem and we will use it to search for the full product
 
   let matchingProduct; // variable to save the result of the search
 
@@ -15,10 +15,7 @@ cart.forEach((cartItem) => {
     }
   });
 
-  console.log(matchingProduct)
-  // we are using the same code in checkout.js and amazon.js we can share this code between the two files by modules
-  // we wont need / 100).toFixed(2) part as we made a function for it in another file
-  // instead of using delivery-option-1 we will use delivery-option-${product.id} so that each product has its own delivery options
+  
   cartSummaryHTML += `
     <div class="cart-item-container
       js-cart-item-container-${matchingProduct.id}">
@@ -104,7 +101,7 @@ document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
 // we will add the event listener to the delete link
 document.querySelectorAll('.js-delete-link').forEach((link) => {
   link.addEventListener('click', () => {
-    //console.log('delete clicked') to make sure its working
+   
     // when we click delete we want to do 2 steps 1. remove product from the cart 2. update the html
 
     const productId = link.dataset.productId // we will get the product id from the data attribute
