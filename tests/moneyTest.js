ec859443325a67ab3e62180088ed367c1431e201
OmarElj <166073351+OmarElhagagy@users.noreply.github.com>
2024-10-25 17:15:25 +0300

    Created automated tests for FormatCurrency

--- tests/moneyTest.js
+++ tests/moneyTest.js
@@ -1,6 +1,6 @@
 import {formatCurrency} from '../scripts/utils/money.js';
-// testing mutiple situations is called test cases
-console.log('test suite: formatCurrency()'); // A group of related tests is called a test suite
+
+console.log('test suite: formatCurrency()'); 
 
 console.log('it converts cents into dollars');
 console.log(formatCurrency(2095) === '20.95' ? 'passed' : 'failed');
@@ -8,17 +8,5 @@ console.log(formatCurrency(2095) === '20.95' ? 'passed' : 'failed');
 console.log('it works with 0');
 console.log(formatCurrency(0) === '0.00' ? 'passed' : 'failed');
 
-// when we calc the 10% tax the result could be a decimal 
 console.log('it rounds to the nearest cent');
 console.log(formatCurrency(2000.5) === '20.01' ? 'passed' : 'failed');
-
-
-/* 
-How many test cases should we have?
-  2 Types of test cases:
-    1. Basic test cases: test if the code is working as expected
-    2. Edge test cases: test with values that are tricky
-     
-*/
-
-// if we removed the Math.round() from the function and re run the test file we will get failed for the last two test cases when we run again
