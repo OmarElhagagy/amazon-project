5322f3b1a7dd061567796370d911e74b8c3772a3
OmarElhagagy <oadeell21@gmail.com>
2024-10-29 01:01:22 +0300

    Make localStorageKey and loadFromStorage private

--- data/cart-class.js
+++ data/cart-class.js
@@ -1,17 +1,18 @@
 // class is an object generator in it we will put the properties and methods that we want for each object we generate from this class
 // class help us generate these objects
+// private property and methods are useful in bigger projects where people might be not sure which properties and methods are supposed to be used outside the class and which are supposed to be used inside the class
 class Cart {
-  cartItems; // add a property to a class every object we generate will have this property 
-  localStorageKey; 
+  cartItems; //public property // add a property to a class every object we generate will have this property 
+  #localStorageKey; //private property this means it can be only used inside this class // this class has a property called localStorageKey this property is useful inside the class but its not meant to be used outside the class so we will make it private so it can be accessed inside the class but not outside the class
 
   constructor(localStorageKey) { // We need to FIX First is each object we create is going to run the constructor so we need one of each of these lines cart or businessCart Second is that the object we create is not going to be called cart every time so we need to replace cart with this so that it refers to the object we generate from the class AND we dont want localStorageKey to be cart-oop every time so we will replace it with a parameter
-    this.localStorageKey = localStorageKey;
-    this.loadFromStorage();
+    this.#localStorageKey = localStorageKey;
+    this.#loadFromStorage();
     
   }
-
-  loadFromStorage() { //function inside an object called Method. There is a shorthand property instead of loadFromStorage: function() we can just write loadFromStorage() {}
-      this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey));// we are loading from the same key in localStorage which is cart-oop every cart we generate it will get its data from same space from localStorage so we will replace it with a parameter //cart.cartItem to access the cartItems values
+  // Method loadFromStorage should also be used inside this class so we will make it private method
+  #loadFromStorage() { //function inside an object called Method. There is a shorthand property instead of loadFromStorage: function() we can just write loadFromStorage() {}
+      this.cartItems = JSON.parse(localStorage.getItem(this.#localStorageKey));// we are loading from the same key in localStorage which is cart-oop every cart we generate it will get its data from same space from localStorage so we will replace it with a parameter //cart.cartItem to access the cartItems values
     
       if (!this.cartItems) {
         this.cartItems = [{ 
@@ -27,7 +28,7 @@ class Cart {
     };
 
     saveToStorage() {
-      localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems)); 
+      localStorage.setItem(this.#localStorageKey, JSON.stringify(this.cartItems)); 
     };
 
     addToCart(productId) {
@@ -85,6 +86,8 @@ class Cart {
 // so the values inside here are saved in the parameters of the constructor so cart-oop will be saved inside localStorageKey
 const cart = new Cart('cart-oop'); // we save a value for the localStorageKey parameter of constructor here // a class uses similar syntax as a function to generate an object from the class we use new keyword
 const businessCart = new Cart('cart-business'); // to generate a new object from the class we use new keyword
+
+
 /*
       Constructor lets us put this setup code inside the class and it works like a normal method we can put code inside and it will run the code but the Special thing about constructor is that when we generate an object  it will run the code inside the constructor automatically
 
