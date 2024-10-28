d448a2a97315fbbd69500d96ae84ab03028d20af
OmarElj <166073351+OmarElhagagy@users.noreply.github.com>
2024-10-28 17:16:40 +0300

    Update moneyTest.js

--- tests/tests-simple/moneyTest.js
+++ tests/tests-simple/moneyTest.js
@@ -1,7 +1,6 @@
 import {formatCurrency} from '../../scripts/utils/money.js';
-// testing mutiple situations is called test cases
-// Testing framework is an external library that helps us write tests easier and helps us do all this things below automatically and gives us many features
-console.log('test suite: formatCurrency()'); // A group of related tests is called a test suite
+
+console.log('test suite: formatCurrency()');
 
 console.log('it converts cents into dollars');
 console.log(formatCurrency(2095) === '20.95' ? 'passed' : 'failed');
