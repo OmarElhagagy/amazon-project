53af106023f7ca482cde0669dbc213c93164b886
OmarElhagagy <oadeell21@gmail.com>
2024-11-01 19:10:39 +0200

    Convert from promises to async await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -5,15 +5,49 @@ import { loadCart } from "../data/cart.js";
 //import '../data/cart-class.js';
 //import '../data/backend-practice.js';
 
+/* async makes a function return a promise its a shortcut for
+function loadPage() {
+  return new Promise((resolve) => {
+    resolve();
+  });
+}
+
+If we write return 'value2' its same as
+function loadPage() {
+  return new Promise((resolve) => {
+    resolve('value2');
+  });
+}
+*/ 
+
+// We use async bec it lets us use await. await lets us wait for promise to finish before going to next line
+async function loadPage() { // async makes a function return a promsie  // as this returns a promise we can add a next step
+  // we loaded the products and waited for it to finish then we load the cart
+  await loadProductsFetch() // async await can only be used with promises // We can only use await when we are inside async function // loads products from the backend. This returns a promise so one way to wait for this to finish is to use .then() and give it a function. Instead we use await to wait for this promise to finish
+
+  const value = await new Promise((resolve) => { // we can use await with Promise.all()
+    loadProducts(() => { 
+      resolve('value3'); 
+    }); 
+  })//.then((value) => {}); value1 will be saved in value parameter. But when we use await this value1 is returned from the promise so we can save it in a variable
+
+  renderOrderSummary();
+  renderPaymentSummary(); 
+}
+loadPage()
+
+
+// Currently we are using promises to handle asynchronous code but problem with promises it creates alot of code. Async await is a shortcut for promises
 // Promise.all 1.lets us run multiple promises at the same time 2. wait for all of them to finish
+/*
 Promise.all([ // An array of promises and we will give this array to Promise.all() and it will wait for all the promises to finish before going to next step
   loadProductsFetch()
-  /* instead of this fetch already returns a promise so we dont need to create a new promise
+   instead of this fetch already returns a promise so we dont need to create a new promise
   new Promise((resolve) => { 
     loadProducts(() => { 
       resolve('value1'); 
     }); 
-  })*/,
+  }),
   new Promise((resolve) => {
     loadCart(() => {
       resolve();
@@ -25,6 +59,7 @@ Promise.all([ // An array of promises and we will give this array to Promise.all
   renderOrderSummary();
   renderPaymentSummary();
 });
+*/
 
 /*
 new Promise((resolve) => { // This inner function gets a parameter called resolve its a function works similar to done() function. Lets us control when to go to the next step // promise is a built in class and when we create a prmise we need to give it a function. When we create this promise it will run this function immediately
